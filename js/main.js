743079e781bf8ce89ffaa24fc0b60ffe34d5fce9
Mirvokhid013 <mirvokhid013@gmail.com>
2023-10-22 22:46:34 +0500

    Changed Scss Styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 AOS.init();
+
+let elBtnSh1 = document.querySelector(".bsh1");
+let elBtnR1 = document.querySelector(".br1");
+let elCpo = document.querySelector(".cpo_depart");
+
+elBtnSh1.addEventListener("click", function(evt){
+    evt.preventDefault();
+    elCpo.style.display = "flex";
+})
+
+elBtnR1.addEventListener("click", function(evt){
+    evt.preventDefault();
+    elCpo.style.display = "none";
+})
+
+let elBtnSh2 = document.querySelector(".bsh2");
+let elBtnR2 = document.querySelector(".br2");
+let elCfo = document.querySelector(".cfo_depart");
+
+elBtnSh2.addEventListener("click", function(evt){
+    evt.preventDefault();
+    elCfo.style.display = "flex";
+})
+
+elBtnR2.addEventListener("click", function(evt){
+    evt.preventDefault();
+    elCfo.style.display = "none";
+})
+
+
